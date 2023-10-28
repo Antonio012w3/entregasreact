cd523bf1023af52c0d0670189826732a44b70642
Antonio012w3 <138320163+Antonio012w3@users.noreply.github.com>
2023-10-28 11:39:17 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a Mi Aplicación React</h1>
+      <p>¡Hola, mundo!</p>
+    </div>
+  );
+}
+
+export default App;
+
